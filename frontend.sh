2dd0c4339583b245ff9145a243d24c846486a5fa
devops <md_syd01@yahoo.co.in>
2020-11-25 16:23:58 +0530

    fourth commit

--- frontend.sh
+++ frontend.sh
@@ -37,7 +37,20 @@ update)
   ;;
 frontend)
   print "Installing nginx server"
+  yum install nginx -y
   status_check
+  print "Starting nginx service"
+  systemctl start nginx
+  systemctl enable nginx
+  print "Downloading the source code"
+  curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/db389ddc-b576-4fd9-be14-b373d943d6ee/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+  status_check
+  cd /usr/share/nginx/html
+  rm -rf  *
+  unzip /tmp/frontend.zip
+  mv static/* .
+  rm -rf static README.md
+  mv localhost.conf /etc/nginx/nginx.conf
   print1 "Finish...."
   ;;
 catalogue)
